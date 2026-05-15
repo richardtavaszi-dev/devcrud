119cce09af7eaa4a29ffead3282fdeff04b0c111
Richard <265088646+richardtavaszi-dev@users.noreply.github.com>
2026-05-15 08:55:51 +0200

    init-components list and nav created

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Nav } from './nav/nav';
+import { List } from './list/list';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Nav, List],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
